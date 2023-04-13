7e02f017663a06d329417f6c6379e14fccd93526
grant <117955357+Kiddeke@users.noreply.github.com>
2023-04-13 10:52:10 -0400

    Add getComputerChoice function

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function getComputerChoice() {
+  let choice = Math.random();
+    if (choice < 0.33) {
+    return "Rock";
+  } else if (choice > 0.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+
+getComputerChoice();
