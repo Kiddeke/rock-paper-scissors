e8af53708683ba3cb98e0a123d79ba33299bbd1f
grant <117955357+Kiddeke@users.noreply.github.com>
2023-04-13 11:39:05 -0400

    Add playRound function

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-
-
 function getComputerChoice() {
   let choice = Math.random();
-    if (choice < 0.33) {
-    return "Rock";
+  if (choice < 0.33) {
+    return "rock";
   } else if (choice > 0.66) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("rock, paper, or scissors?", "").toLowerCase();
+  computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    return alert("TIE");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return alert("You Lose. Paper beats Rock");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return alert("You Win. Paper beats Rock");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return alert("You Win. Rock beats Scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return alert("You Lose. Rock beats Scissors");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return alert("You Lose. Scissors beats Paper");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return alert("You Win. Scissors beats Paper");
+  } else {
+    alert("please choose rock, paper or scissors");
+  }
+}
