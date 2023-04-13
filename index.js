538ebfc5d5b901c4debd9cad48a5f1fe2a570321
grant <117955357+Kiddeke@users.noreply.github.com>
2023-04-13 11:48:45 -0400

    Add formatting

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = prompt("rock, paper, or scissors?", "").toLowerCase();
   computerSelection = getComputerChoice();
   if (playerSelection === computerSelection) {
-    return alert("TIE");
+    return alert("TIE!");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return alert("You Lose. Paper beats Rock");
+    return alert("You Lose. Paper beats Rock!");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return alert("You Win. Paper beats Rock");
+    return alert("You Win. Paper beats Rock!");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return alert("You Win. Rock beats Scissors");
+    return alert("You Win. Rock beats Scissors!");
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return alert("You Lose. Rock beats Scissors");
+    return alert("You Lose. Rock beats Scissors!");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return alert("You Lose. Scissors beats Paper");
+    return alert("You Lose. Scissors beats Paper!");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return alert("You Win. Scissors beats Paper");
+    return alert("You Win. Scissors beats Paper!");
   } else {
     alert("please choose rock, paper or scissors");
   }
